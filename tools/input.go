89f1827985950af328bf3672de68f7c2303d97c0
atarte <tarte.antoine@gmail.com>
2024-04-08 21:59:02 +0200

    Add function to manage choice

---
+++ tools/input.go
@@ -0,0 +1,82 @@
+package tools
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+// ConfirmInput
+func ConfirmInput(question string) bool {
+	fmt.Printf("%s (y/n)\n", question)
+	// input =
+	return true
+}
+
+type ChoiceElement struct {
+	Text  string
+	Input string
+	Exec  func()
+}
+
+// isChoiceInputFromList
+func isChoiceInputFromList(choiceList []ChoiceElement) bool {
+	for _, choice := range choiceList {
+		if choice.Input == "" {
+			return false
+		}
+	}
+	return true
+}
+
+// ChoiceInput
+func ChoiceInput(question string, choiceList []ChoiceElement) error {
+	fmt.Printf("%s\n", question)
+
+	if !isChoiceInputFromList(choiceList) {
+		return errors.New("Missing `input`field in choice element")
+	}
+
+	for _, choice := range choiceList {
+		fmt.Printf("(%s) - %s\n", choice.Input, choice.Text)
+	}
+
+	var input string
+	fmt.Scanln(&input)
+
+	for _, choice := range choiceList {
+		if input == choice.Input {
+			choice.Exec()
+			break
+		}
+	}
+
+	return nil
+}
+
+// ChoiceNumberInput
+func ChoiceNumberInput(question string, choiceList []ChoiceElement, offset bool) {
+	fmt.Printf("%s\n", question)
+
+	for i, choice := range choiceList {
+		if offset {
+			i++
+		}
+
+		fmt.Printf("(%d) - %s\n", i, choice.Text)
+	}
+
+	var input string
+	fmt.Scanln(&input)
+
+	for i, choice := range choiceList {
+		if offset {
+			i++
+		}
+
+		if input == strconv.Itoa(i) {
+			choice.Exec()
+			break
+		}
+	}
+}
