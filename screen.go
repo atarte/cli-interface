65ef43fca7ced563c083eab024b11b90c3f40894
atarte <tarte.antoine@gmail.com>
2024-04-18 15:18:54 +0200

    merge screen file

--- screen.go
+++ screen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -10,4 +11,11 @@ func ClearScreen() {
 	cmd := exec.Command("clear")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
+}
+
+// WaitScreen
+func WaitScreen() {
+	fmt.Println("")
+	fmt.Println("(Press enter to return...)")
+	fmt.Scanln()
 }
